268f284137707a701b8525b0184ea7bc73ccdee1
aishamohamed <aishac@chalmers.se>
2024-01-30 14:54:38 +0100

    Lab 2

---
+++ Flake.java
@@ -0,0 +1,31 @@
+public class Flake {
+    private int angle;
+    public Flake (int angle) {
+        this.angle = angle;
+    }
+
+    public int getAngle() {
+        return angle;
+    }
+
+    public void setAngle(int angle) {
+        this.angle = angle;
+        if (!isWithinRange()) {
+            throw new IllegalArgumentException("Is not within range: " + angle);
+        }
+    }
+    boolean isWithinRange() {
+        return this.angle >= 0 && this.angle <= 70;
+    }
+    void increaseAngle() {
+        if (isWithinRange() && angle != 70) {
+            angle++;
+        }
+    }
+    void decreaseAngle() {
+        if (isWithinRange() && angle != 0) {
+            angle--;
+        }
+    }
+
+}
---
+++ ScaniaTest.java
@@ -0,0 +1,46 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ScaniaTest {
+    Scania scania;
+    @BeforeEach
+    void setUp() {
+        scania = new Scania();
+    }
+
+    @Test
+    void speedFactor() {
+    }
+
+    @Test
+    void incrementSpeed() {
+    }
+
+    @Test
+    void decrementSpeed() {
+    }
+
+    @Test
+    void decreaseFlake() {
+    }
+
+    @Test
+    void increaseFlake() {
+        scania.currentSpeed = 0;
+        scania.increaseFlake();
+        System.out.print(scania.getFlake().getAngle());
+    }
+
+    @Test
+    void move() {
+        int v = scania.getFlake().getAngle();
+         v = 30;
+         scania.currentSpeed = 30;
+         scania.move();
+         System.out.print(scania.getCurrentSpeed());
+    }
+
+
+}
